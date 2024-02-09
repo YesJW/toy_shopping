1379877a13955ea0077aae6c7fa147ef7f9d735e
jangwoo <ssk01017@gmail.com>
2024-02-09 22:58:55 +0900

    @ResponseBody 추가

--- src/main/java/com/toyshopping/toy_shopping/controller/MyPageController.java
+++ src/main/java/com/toyshopping/toy_shopping/controller/MyPageController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -38,7 +39,9 @@ public class MyPageController {
         return "/sales";
     }
 
+
     @GetMapping("/user_detail")
+    @ResponseBody
     public UsersDto getUser(Principal principal){
         LOGGER.info("[getUser] getUser 메서드 호출됨.");
         UsersDto userDto = new UsersDto();
