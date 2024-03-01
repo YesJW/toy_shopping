402058e16c939a30394870ad9d87c0374e70e322
jangwoo <ssk01017@gmail.com>
2024-03-01 23:35:40 +0900

    feat :  상단 바 addproductPage를 getProductPage로 변경

--- src/main/java/com/toyshopping/toy_shopping/repository/CartProductRepository.java
+++ src/main/java/com/toyshopping/toy_shopping/repository/CartProductRepository.java
@@ -3,9 +3,13 @@ package com.toyshopping.toy_shopping.repository;
 import com.toyshopping.toy_shopping.data.entity.CartProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface CartProductRepository extends JpaRepository<CartProduct, Long> {
 
     CartProduct findByCart_cNumAndProduct_numb(Long cartId, Long pId);
+
+    List<CartProduct> findByProduct_numb(Long id);
 
 }
