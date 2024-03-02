a9f1f2ef59aaa6db3adf79b95806e1d9960351e2
jangwoo <ssk01017@gmail.com>
2024-03-03 00:51:03 +0900

    feat : 상품 구매로 인한 수정 및 추가
    상품 구매 시 기존의 판매 상품의 갯수 감소, 상품구매번호를 현재 시간으로 수정,
    쇼핑카트에서 구매한 상품 제거

--- src/main/java/com/toyshopping/toy_shopping/service/OrderService.java
+++ src/main/java/com/toyshopping/toy_shopping/service/OrderService.java
@@ -1,27 +1,18 @@
 package com.toyshopping.toy_shopping.service;
 
 import com.toyshopping.toy_shopping.data.dto.CartProductDto;
-import com.toyshopping.toy_shopping.data.dto.OrderProductDto;
-import com.toyshopping.toy_shopping.data.entity.CartProduct;
-import com.toyshopping.toy_shopping.data.entity.Order;
-import com.toyshopping.toy_shopping.data.entity.OrderProduct;
-import com.toyshopping.toy_shopping.data.entity.Users;
-import com.toyshopping.toy_shopping.repository.OrderProductRepository;
-import com.toyshopping.toy_shopping.repository.OrderRepository;
-import com.toyshopping.toy_shopping.repository.UserRepository;
+import com.toyshopping.toy_shopping.data.entity.*;
+import com.toyshopping.toy_shopping.repository.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
-import static com.fasterxml.jackson.databind.type.LogicalType.DateTime;
 
 @Service
 public class OrderService {
@@ -29,48 +20,66 @@ public class OrderService {
     private UserRepository userRepository;
     private OrderProductRepository orderProductRepository;
 
-    public OrderService(OrderRepository orderRepository, UserRepository userRepository, OrderProductRepository orderProductRepository) {
+    private ShoppingCartService shoppingCartService;
+
+    private CartProductRepository cartProductRepository;
+    private ProductRepository productRepository;
+
+    public OrderService(OrderRepository orderRepository, UserRepository userRepository,
+                        OrderProductRepository orderProductRepository, ShoppingCartService shoppingCartService,
+                        CartProductRepository cartProductRepository, ProductRepository productRepository) {
+        this.productRepository = productRepository;
+        this.cartProductRepository = cartProductRepository;
+        this.shoppingCartService = shoppingCartService;
         this.orderProductRepository = orderProductRepository;
         this.orderRepository = orderRepository;
         this.userRepository = userRepository;
     }
 
-    public Order createOrder(Users users) {
-        Order order = Order.createdOrder(users);
-
-        return order;
-
+    public Orders createOrder(Users users) {
+        Orders orders = Orders.createdOrder(users);
+        return orders;
     }
 
     @Transactional
-    public void buyCartProducts(List<CartProductDto> cartProductDtoList) {
+    public void buyCartProducts(List<CartProductDto> cartProductDtoList) throws Exception {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Users users = userRepository.getByUid(authentication.getName());
-        Order order = createOrder(users);
-        orderRepository.save(order);
-        List<OrderProduct> orderProducts = addOrderProducts(cartProductDtoList, order, users);
-        order.setOrderProducts(orderProducts);
-        orderRepository.save(order);
+        Orders orders = createOrder(users);
+        orderRepository.save(orders);
+        List<OrderProduct> orderProducts = addOrderProducts(cartProductDtoList, orders, users);
+        orders.setOrderProducts(orderProducts);
+        orderRepository.save(orders);
+
+
+        for (CartProductDto cartProductDto : cartProductDtoList) {
+            Product product = productRepository.getById(cartProductDto.getProductId());
+            product.setStock(product.getStock() - cartProductDto.getCount());
+            productRepository.save(product);
+        }
+
     }
 
 
 
-    public List<OrderProduct> addOrderProducts(List<CartProductDto> cartProductDtos, Order order, Users users) {
+    public List<OrderProduct> addOrderProducts(List<CartProductDto> cartProductDtos, Orders orders, Users users) throws Exception {
         List<OrderProduct> orderProducts = new ArrayList<>();
-
         for (CartProductDto cartProductDto : cartProductDtos) {
+            LocalDateTime localDateTime = LocalDateTime.now();
+            String localDateTimeFormat1
+                    = localDateTime.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS"));
             OrderProduct orderProduct = OrderProduct.builder()
-                    .order(order)
+                    .orders(orders)
                     .price(cartProductDto.getPrice())
                     .productName(cartProductDto.getProductName())
                     .total(cartProductDto.getPrice() * cartProductDto.getCount())
-                    .orderId((int) System.currentTimeMillis())
+                    .orderNumber(localDateTimeFormat1)
                     .count(cartProductDto.getCount())
                     .users(users)
                     .build();
             orderProductRepository.save(orderProduct);
             orderProducts.add(orderProduct);
-
+            shoppingCartService.deleteShoppingCartProduct(cartProductDto.getId());
         }
         return orderProducts;
     }
