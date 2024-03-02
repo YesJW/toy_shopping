0793064aba95538d546536fe01d435f978ce7750
jangwoo <ssk01017@gmail.com>
2024-03-03 00:48:02 +0900

    feat : 상품 구매 기능 추가

--- src/main/java/com/toyshopping/toy_shopping/controller/OrderController.java
+++ src/main/java/com/toyshopping/toy_shopping/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.toyshopping.toy_shopping.controller;
 import com.toyshopping.toy_shopping.data.dto.CartProductDto;
 import com.toyshopping.toy_shopping.data.dto.ProductDto;
 import com.toyshopping.toy_shopping.data.entity.OrderProduct;
+import com.toyshopping.toy_shopping.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,11 +17,13 @@ import java.util.List;
 @RestController
 public class OrderController {
 
-    @PostMapping("/buyProduct")
-    public ResponseEntity<List<OrderProduct>> buyProduct(@RequestBody List<CartProductDto> cartProductDtos) {
 
+    @Autowired
+    private OrderService orderService;
 
-
+    @PostMapping("/buyProducts")
+    public ResponseEntity<List<OrderProduct>> buyProduct(@RequestBody List<CartProductDto> cartProductDtos) throws Exception {
+        orderService.buyCartProducts(cartProductDtos);
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
