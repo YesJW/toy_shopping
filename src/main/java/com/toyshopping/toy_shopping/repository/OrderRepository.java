51c4e5e7be5876f9aaaa405c553fdc41b4217b90
jangwoo <ssk01017@gmail.com>
2024-03-03 23:14:51 +0900

    feat : user id로 Orders 조회

--- src/main/java/com/toyshopping/toy_shopping/repository/OrderRepository.java
+++ src/main/java/com/toyshopping/toy_shopping/repository/OrderRepository.java
@@ -3,9 +3,5 @@ package com.toyshopping.toy_shopping.repository;
 import com.toyshopping.toy_shopping.data.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface OrderRepository extends JpaRepository<Orders, Long> {
-    List<Orders> findAllByUsers_id(Long id);
-
 }
