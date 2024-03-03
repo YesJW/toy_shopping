f5fd13acc78265ab9214af5e70690824ca536d60
jangwoo <ssk01017@gmail.com>
2024-03-03 23:15:41 +0900

    refactor : LocalDate Format 변경
    yyyy-MM-dd -> yyyy-MM-dd HH:mm:ss

--- src/main/java/com/toyshopping/toy_shopping/data/entity/Orders.java
+++ src/main/java/com/toyshopping/toy_shopping/data/entity/Orders.java
@@ -25,7 +25,7 @@ public class Orders {
     @JoinColumn(name = "user_id")
     private Users users;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
     private LocalDate time;
 
     @OneToMany(mappedBy = "orders")
