5cdb5674d33582879b467c3eec04ba6c64d848a7
jangwoo <ssk01017@gmail.com>
2024-03-03 22:58:38 +0900

    feat : 구매 목록 페이지 이동 기능 추가

--- src/main/java/com/toyshopping/toy_shopping/repository/OrderRepository.java
+++ src/main/java/com/toyshopping/toy_shopping/repository/OrderRepository.java
@@ -3,5 +3,9 @@ package com.toyshopping.toy_shopping.repository;
 import com.toyshopping.toy_shopping.data.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Orders, Long> {
+    List<Orders> findAllByUsers_id(Long id);
+
 }
